52418e5b18a56a0715112556aa808c3218dbe0ba
Omid <omiddagala@yahoo.com>
2017-06-04 00:04:51 +0430

    ...

--- src/services/my.service.ts
+++ src/services/my.service.ts
@@ -32,7 +32,6 @@ export class MyService {
   }
 
   private handleError (error: Response | any) {
-    // In a real world app, you might use a remote logging infrastructure
     let errMsg: string;
     if (error instanceof Response) {
       const body = error.json() || '';
